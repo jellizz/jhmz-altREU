2c8eaf4726fb9283fbd761759f1638ff59bd3b22
marlazacks <marlazacks05@gmail.com>
2026-06-28 17:38:29 -0400

    code for getting abstracts

---
+++ abstract_pull.py
@@ -0,0 +1,5 @@
+"""
+Uses the OpenAlex API to pull paper abstracts from OpenAlex and store them in a local database.
+This code specifically tests pulling 30 abstracts from Nature Physics to test the functionality of the OpenAlex API and the database storage.
+"""
+
