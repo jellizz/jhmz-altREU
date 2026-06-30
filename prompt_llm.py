cb5a67e86350e6084e3a5a68467da142690ccde1
Judy <jzh24@cornell.edu>
2026-06-29 23:37:50 -0400

    Completed main loop for prompt_llm; tested with hardcoded data (the test... json files). Modified requirements.

--- prompt_llm.py
+++ prompt_llm.py
@@ -18,12 +18,15 @@ Feeds each prompt to the LLM and stores its responses as a JSON.
 
 import os
 import json
+from dotenv import load_dotenv
+load_dotenv()
 
 from openai import OpenAI
 from llama_api_client import LlamaAPIClient
 from anthropic import Anthropic
 from google import genai
 
+'''
 openai_client = OpenAI(
     # This is the default and can be omitted
     api_key=os.environ.get("OPENAI_API_KEY"),
@@ -38,15 +41,17 @@ anthropic_client = Anthropic(
     api_key=os.environ.get("ANTHROPIC_API_KEY"),
 )
 
-gemini_client = genai.Client()
+gemini_client = genai.Client()'''
 
 
 prompt_base_tsk1 = "TASK 1: words..."
 prompt_base_tsk2 = "TASK 2: more words..."
+abstracts_file = "test_prompt_llm.json"
 
 
 # 1a. Load current JSON, if it exists. Build off of this.
 # (Safeguard for if it crashes in the middle, can pick up where it left off)
+
 def load_file(filename):
     if os.path.exists(filename):
         with open(filename, "r") as f:
@@ -57,6 +62,7 @@ def load_file(filename):
 # 1b. Parse the abstracts JSON for an abstract; cross-check ids of abstract
 # JSON and current JSON being build to ensure that an abstract
 # has not been prompted with yet
+
 def completed_abstract_ids(filename):
     content = load_file(filename)
     completed_abstracts = []
@@ -66,8 +72,10 @@ def completed_abstract_ids(filename):
 
 
 # 2. Inject the abstract into the prompt base
+
 def build_prompt(abstract):
     return f"{prompt_base_tsk1} Abstract: {abstract} {prompt_base_tsk2}"
+
 
 # 3. Make call to LLM API to prompt
     # determine which LLM is being prompted
@@ -86,6 +94,7 @@ def build_prompt(abstract):
         #response.content[0].text
 
         # Gemini
+            # https://ai.google.dev/gemini-api/docs/get-started 
         #response.output_text
 
 def prompt_llm(prompt, llm):
@@ -134,12 +143,13 @@ def prompt_llm(prompt, llm):
         )
         return response.output_text
     else:
-        print("something bad happened oh no")
+        return "QUESTION:\nWhat is a pineapple?\nANSWER:\nA fruit.\nREFERENCES:\n[1] Doe, John. (2026). Pineapple. Pineapple Journal, 1(23), 45-67. DOI: pineapple.com.\n[2] Doe, Jane. (1234). Original Pineapple. Pineapple Origins, 1(23), 45-67. DOI: pineapple.og.com."
         
 
 # 4. Store response, along with other info, into new JSON
 # breaks the response into 'question', 'answer', 'references'
 # !!! NEED TO TEST LLM RESPONSE to see if it actually formats how we want, bc this depends on the llm response format
+
 def parse_response(response):
     '''
     say the LLM's response looks like this:
@@ -151,12 +161,13 @@ def parse_response(response):
         references."
     '''
     temp1 = response.split("QUESTION:\n") # ["", "wordy words. ANSWER: words, words. REFERENCES: references."]
-    temp2 = temp1[1].split("ANSWER:\n") # ["wordy words.", "words, words. REFERENCES: references."]
-    temp3 = temp2[1].split("REFERENCES:\n") # ["words, words.", "references."]
+    temp2 = temp1[1].split("\nANSWER:\n") # ["wordy words.", "words, words. REFERENCES: references."]
+    temp3 = temp2[1].split("\nREFERENCES:\n") # ["words, words.", "references."]
     question = temp2[0]
     answer = temp3[0]
     references = temp3[1]
     return question, answer, references
+
 
 def build_json(filename, abstract_id, llm, question, answer, references):
     output_file = load_file(filename)
@@ -175,5 +186,25 @@ def build_json(filename, abstract_id, llm, question, answer, references):
     with open(filename, "w") as f:
         json.dump(output_file, f)
 
+
 # 5. Repeat
 # write main loop in here, later combine everything outside in main.py?
+
+if __name__ == "__main__":
+    llm = "testing" # update for different LLMs
+    abstracts = load_file(abstracts_file)
+    output_file = "test_responses.json" # update name for different LLMs
+    completed_abstracts = completed_abstract_ids(output_file)
+
+    for abstract in abstracts:
+        abstract_id = abstract["id"]
+        if abstract_id in completed_abstracts:
+            continue
+
+        prompt = build_prompt(abstract["abstract"])
+        response = prompt_llm(prompt, llm)
+        question, answer, references = parse_response(response)
+        build_json(output_file, abstract_id, llm, question, answer, references)
+
+# This currently spits out one big string... probably should fix format
+# Also, it seems to work with hardcoded data. Dunno about LLM calls.
